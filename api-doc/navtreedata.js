eb3938af1f5e26c189fb1b9ac82d5e370109565c
publish-api-doc GitHub action <libyui-github-bot@suse.com>
2021-11-11 15:46:56 +0000

    Generated API doc

---
+++ api-doc/navtreedata.js
@@ -0,0 +1,69 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "libyui", "index.html", [
+    [ "Notes about Initialization", "notes1.html", [
+      [ "entry points", "notes1.html#Important", null ],
+      [ "happens on initialization", "notes1.html#What", null ],
+      [ "classes", "notes1.html#Utility", null ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Related Functions", "functions_rela.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"FSize_8h_source.html",
+"classYBarGraphSegment.html#a33b7b649dedf612d1d9bf38e8fb5b3ac",
+"classYDownloadProgress.html#a6855b05aabd1812f0df6e70ed835750f",
+"classYLayoutBox.html#ac8024a08ecd51adb1212f0882cde65a9",
+"classYRadioButton.html#a95a53e14d5d2dc51e93b3907206e1d04",
+"classYTable.html#a647fe941a154b63cf480f723b9ca809c",
+"classYWidget.html#a5e393b10927a66f82abbe89ebb4e354a"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ api-doc/search/classes_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['implptr_0',['ImplPtr',['../classImplPtr.html',1,'']]]
+];
